889addd26981608d6342f2553275bb8722bdc3cc
Muhsinws <muhsin@weamse.in>
2025-02-21 17:22:55 +0530

    email cre

--- my_project/settings.py
+++ my_project/settings.py
@@ -309,8 +309,14 @@ SOCIALACCOUNT_PROVIDERS = {
     }
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'muhsytest@gmail.com'
+EMAIL_HOST_PASSWORD = 'coexaqssuyyfkqnw'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_EMAIL_REQUIRED = True
 # ACCOUNT_LOGIN_BY_CODE_ENABLED = True
